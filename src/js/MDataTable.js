300fcc01e182ae5ee8c79e57d5e4c526abdd6675
Mehmet Can Kızılyer <MehmetCan.Kizilyer@d-teknoloji.com.tr>
2019-08-27 15:36:56 +0300

    Calc index event is added

--- src/js/MDataTable.js
+++ src/js/MDataTable.js
@@ -50,10 +50,10 @@ export default class MDataTable {
     deleteEventManipulation (item) {
         this.item = item;
         this.row = this.item.parentNode.parentNode;
-
+        
         // Initialize warn operations
         [ this.approveBtn, this.cancelBtn, this.warnDom ] = MDataTable.WarnOperation(this.row, this.local);
-
+        
         // Delete operation button events
         
         /*
@@ -62,15 +62,19 @@ export default class MDataTable {
         this.approveBtn.onclick = () => {
             // Delete warn dom
             this.warnDom.parentNode.removeChild(this.warnDom);
-
+            
             // Delete row
             this.row.classList.add('delete-operation-start');
-
+            
             setTimeout(() => {
                 this.row.parentNode.removeChild(this.row);
+                
+                // Change count Index
+                this.calcCountIndex();
+                
             }, 501); // animation ended is remove row
         }
-
+        
         /*
         * Cancel Button
         */
@@ -78,7 +82,7 @@ export default class MDataTable {
             this.warnDom.parentNode.removeChild(this.warnDom);
         }
     }
-
+    
     static WarnOperation (row, local) {
         // Table row
         this.row = row;
@@ -87,7 +91,7 @@ export default class MDataTable {
         // Initialize warn dom
         return MDataTable.CreateWarnDom(this.rect.top, this.rect.left, this.row.clientWidth, this.row.clientHeight, local);
     }
-
+    
     static CreateWarnDom (top, left, width, height, local) {
         // Distances & Height
         this.topDistance = top;
@@ -95,7 +99,7 @@ export default class MDataTable {
         this.rowWidth = width;
         this.rowHeight = height;
         this.local = local;
-
+        
         if ( document.body.getElementsByClassName('m-table-warn').length !== 0 ) return;
         
         // Creating warn dom
@@ -103,7 +107,7 @@ export default class MDataTable {
         warnDom.classList.add('m-table-warn', 'delete-operation');
         warnDom.style.left = `${this.leftDistance + (this.rowWidth / 3.5)}px`;
         warnDom.style.top = `${this.topDistance + this.rowHeight}px`;
-
+        
         // Creating warn dom header
         const warnDomHeader = document.createElement('div');
         warnDomHeader.classList.add('m-table-warn-header');
@@ -113,23 +117,23 @@ export default class MDataTable {
         warnDomHeaderContent.classList.add('m-table-warn-header-content');
         warnDomHeaderContent.innerText = this.local.deleteWarn.title ? this.local.deleteWarn.title : 'Silmek istediğine emin misin ?';
         warnDomHeader.appendChild(warnDomHeaderContent);
-
+        
         warnDom.appendChild(warnDomHeader);
-
+        
         // Creating Action Buttons
         
         const buttonContainer = document.createElement('div');
         buttonContainer.classList.add('m-table-button-container');
-         
+        
         const approveButton = document.createElement('button');
         const cancelButton = document.createElement('button');
-
+        
         approveButton.innerText = this.local.deleteWarn.approveBtn ? this.local.deleteWarn.approveBtn : 'Silmek istediğine emin misin ?';;
         cancelButton.innerText = this.local.deleteWarn.cancelBtn ? this.local.deleteWarn.cancelBtn : 'Silmek istediğine emin misin ?';;
         
         approveButton.classList.add('approve-button');
         cancelButton.classList.add('btn-cancel');
-
+        
         warnDom.appendChild(buttonContainer);
         
         buttonContainer.appendChild(approveButton);
@@ -137,10 +141,10 @@ export default class MDataTable {
         
         // Append Dom
         document.body.append(warnDom);
-
+        
         return [approveButton, cancelButton, warnDom];
     }
-
+    
     editEvent () {
         
         if (this.obj.multi) {
@@ -165,7 +169,7 @@ export default class MDataTable {
         }
         
     }
-
+    
     dbClickEventManipulation (column, type) {
         this.column = column;
         this.type = type;
@@ -178,12 +182,12 @@ export default class MDataTable {
         createInput.value = this.column.innerText;
         this.column.innerText = '';
         this.column.append(createInput);
-
+        
         // Create and add change button
         const createButton = document.createElement('button');
         createButton.innerText = this.local.changeButtonValue ? this.local.changeButtonValue : 'Change';
         this.column.append(createButton);
-
+        
         // Create and add cancel button
         const cancelButton = document.createElement('button');
         cancelButton.innerText = this.local.cancelButtonValue ? this.local.cancelButtonValue : 'Cancel';
@@ -201,13 +205,13 @@ export default class MDataTable {
         this.input = input;
         this.column.innerHTML = this.input.value;
     }
-
+    
     changeInputCancelEventHandler (column) {
         this.column = column;
-
+        
         this.column.innerHTML = this.states.lastColumnValue;
     }
-
+    
     changeInputKeyEventHandler (event, column, input) {
         this.event = event;
         this.column = column;
@@ -218,6 +222,28 @@ export default class MDataTable {
             this.createButtonClickEventHandler(this.column, this.input);
         } else if ( this.event.keyCode === 27 ) {
             this.changeInputCancelEventHandler(this.column);
+        }
+    }
+    
+    calcCountIndex () { // Calc Index
+        if ( this.obj.multi ) {
+            // Multiple Table
+            for (let i = 0; i < this.table.length; i += 1) {
+                this.index = this.table[i].querySelectorAll('tbody th');
+                
+                for (let j = 0; j < this.index.length; j += 1) {
+                    const elem = this.index[j];
+                    elem.innerText = j + 1;
+                }
+            }
+        } else {
+            // Single table
+            this.index = this.table.querySelectorAll('tbody th');
+            
+            for ( let i = 0; i < this.index.length; i += 1 ) {
+                const elem = this.index[i];
+                elem.innerText = i + 1;
+            }
         }
     }
 }
